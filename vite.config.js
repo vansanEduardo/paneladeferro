d023e950fd3795410745c58ab0299f428950189c
vansanEduardo <eduvansan14@gmail.com>
2024-12-06 19:31:52 -0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/paneladeferro/', // Este é o nome exato do repositório no GitHub
+  build: {
+    outDir: 'build', // Certifica que a saída será na pasta 'build'
+  },
+});
